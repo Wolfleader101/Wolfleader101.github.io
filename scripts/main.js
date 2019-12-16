35d15ac8aebbe3ab31a06dee801d766db045c08d
Wolfleader101 <james.morgan@iinet.net.au>
2019-12-16 16:36:33 +0800

    Fullpage.js working

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,20 @@
 new fullpage('#fullpage', {
-    licenseKey: '9B56D9A6-015E49C1-99039BAD-CA082B96',
-    autoScrolling:true,
-    scrollHorizontally: true
-  });
+  licenseKey: '9B56D9A6-015E49C1-99039BAD-CA082B96',
+  autoScrolling: true,
+  navigation: true,
+  scrollHorizontally: true,
+  slidesNavigation: true,
+  controlArrows: false,
+  anchors: ['page1', 'page2', 'page3', 'page4'],
+  navigationTooltips: ['Wolf Studios', 'About Us', 'Projects', 'Blogs', 'Contact Us'],
+  afterRender: () => {
+    setInterval(function () {
+      fullpage_api.moveSlideRight();
+    }, 5000);
+  }
+});
+
+//Read more button click
+$(document).on('click', '#ReadMore', () => {
+  fullpage_api.moveTo('page2');
+});
